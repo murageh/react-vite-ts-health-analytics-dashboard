fd35b2068a9ffd514aab02d124f29d8ff1e3f9e7
MURAGEH <muragehdeveloper@gmail.com>
2024-09-26 20:33:17 +0300

    add apollo-client - add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const client = new ApolloClient({
+        uri: 'http://localhost:4000/', // Replace with your GraphQL API URL
+        cache: new InMemoryCache()
+    });
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>
+  </React.StrictMode>,
 )
